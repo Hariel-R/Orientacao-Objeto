18e18af60b14f6108e01e849e213f54bf365f9d0
Dio <Dio@ACERNITRO-5>
2024-12-12 11:30:03 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var usuario = new UsuarioSousaRibeiro(0, "hariel", new DateTime(2003,02,20), "Rua asturias 678");
+            usuario.ApEndereco();
+            usuario.AltEndereco(Console.ReadLine());
+            usuario.ApEndereco();
+            usuario = new UsuarioSousaRibeiro(0, "gleison", new DateTime(2003, 02, 20), "Rua asturias 678");
+            usuario.ApEndereco();
+        }
+
+        public class Usuario
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public DateTime DataNascimento { get; set; }
+            public string Endereco { get; set; }
+
+            public void AltNome(string novoNome)
+            {
+                this.Nome = novoNome;
+            }
+
+            public void AltDataNascimento(DateTime novaData)
+            {
+                this.DataNascimento = novaData;
+            }
+
+            public void AltEndereco(string novoEndereco)
+            {
+                this.Endereco = novoEndereco;
+            }
+
+            public void ApNome()
+            {
+                Console.WriteLine(Nome);
+            }
+
+            public void ApIdade()
+            {
+                DateTime data = this.DataNascimento;
+                DateTime dataAtual = DateTime.Now;
+                int Idade =  data.Year - dataAtual.Year;
+                if (dataAtual.Month < data.Month || (dataAtual.Month == data.Month && dataAtual.Day < data.Day))
+                {
+                    Idade--; 
+                }
+                Console.WriteLine($"Idade: {Idade} Anos");
+            }
+
+            public void ApEndereco()
+            {
+                Console.WriteLine(Endereco);
+            }
+            
+
+        }
+
+        public class UsuarioSousaRibeiro : Usuario
+        {
+            public UsuarioSousaRibeiro(int id, string nome, DateTime dataNascimento, string endereco) 
+            {
+                this.Id = id;
+                this.Nome = nome;
+                this.DataNascimento = dataNascimento;
+                this.Endereco = endereco;
+            
+            }
+           private List<string> Cargos { get; set; } = new List<string>();
+           private List<string> Perms { get; set; } = new List<string>();
+
+            public void ApCargos()
+            {
+            Console.WriteLine($"Cargos: [{string.Join(", ", this.Cargos)}]");
+            }
+
+            public void InserirCargos()
+            {
+
+                List<string> cargosAdd = new List<string>();
+                string resposta = "n";
+                while (resposta != "N" || resposta != "n")
+                {
+                    cargosAdd.Add(Console.ReadLine());
+                    Console.WriteLine("Deseja adicionar outro cargo? (S/N)");
+                    resposta = Console.ReadLine();
+                }
+
+                Console.WriteLine($"Cargos a serem adicionados: {string.Join(", ", cargosAdd)}");
+                Console.WriteLine("Deseja adicionar? (S/N)");
+                resposta = Console.ReadLine();
+                if (resposta == "S" || resposta == "s") {
+                    this.Cargos.AddRange(cargosAdd);
+                    Console.WriteLine("Cargos inseridos com sucesso");
+                } 
+                else
+                {
+                    Console.WriteLine("Cargos descartados!");
+                }
+            }
+            public void InserirPerms()
+            {
+                this.Perms.Add(Console.ReadLine());
+            }
+            public void RemoveCargo()
+            {
+                this.Cargos.RemoveAt(int.Parse(Console.ReadLine()));
+            }
+            public void RemovePerm()
+            {
+                this.Perms.RemoveAt(int.Parse(Console.ReadLine()));
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing;
+using System.Linq;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+
+            Console.WriteLine("Informe 10 numeros: ");
+
+            for (int i = 1; i <= 10; i++) 
+            { 
+            Console.WriteLine($"Informe o {i}º numero: ");       
+            numeros.Add(int.Parse(Console.ReadLine()));
+                Console.Clear();
+             };
+            var numerosdec = numeros.OrderByDescending(n => n).ToList();
+
+            Console.WriteLine($"Lista: [{string.Join(", ", numerosdec)}]");
+            Console.WriteLine($"Menor valor da lista: {numeros.Min()}");
+            Console.WriteLine($"Maior valor da lista: {numeros.Max()}");
+
+            Console.WriteLine("Insira mais 4 numeros: ");
+            for (int j=0; j<4; j++)
+            {
+                int valor = int.Parse(Console.ReadLine());
+            if ((valor % 2) == 0) {
+                numerosdec.Insert(0, valor);
+            }
+            else
+            {
+                numerosdec.Add(valor);
+            }
+
+            }
+
+            Console.WriteLine($"Lista com novos valores: [{string.Join(", ", numerosdec)}]");
+
+            Console.WriteLine("Informe um valor para remover: ");
+            int valor2 = int.Parse(Console.ReadLine());
+            var indices = numerosdec
+                .Select((num, index) => new { num, index }) 
+                .Where(x => x.num == valor2)               
+                .Select(x => x.index)               
+                .ToList();
+              Console.WriteLine($"Indices do valor {valor2} [ {string.Join(", ", indices)}]");
+            
+            Console.WriteLine("Informe a posição do item que deseja remover: ");
+            numerosdec.RemoveAt(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Lista com valor removido [{string.Join(", ", numerosdec)}]");
+
+        }
+    }
+}
